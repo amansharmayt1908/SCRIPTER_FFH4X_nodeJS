6cd3daf504e7fcfac901c1e768e980fb3fa66274
Aman Sharma <166890222+amansharmayt1908@users.noreply.github.com>
2025-01-16 19:37:34 +0530

    Add files via upload

--- public/free_stuff.js
+++ public/free_stuff.js
@@ -1,4 +1,7 @@
 document.querySelector("#stuff-1").addEventListener("click", () => {
     window.location.href = 'https://just2earn.com/z7mH';
 });
+document.querySelector("#stuff-2").addEventListener("click", () => {
+    window.location.href = 'https://just2earn.com/JPDx';
+});
 
